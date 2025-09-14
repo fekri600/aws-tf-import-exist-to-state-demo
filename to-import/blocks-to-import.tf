a38b6fcd8bf09a70d5cab910571b30dafd5bd2f8
Fekri Saleh <fekri600@gmail.com>
2025-09-13 23:21:34 -0600

    add debug comments  in pipeline

---
+++ to-import/blocks-to-import.tf
@@ -0,0 +1,119 @@
+resource "aws_iam_role" "AWSServiceRoleForAmazonEKS" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAmazonEKS
+  name = "AWSServiceRoleForAmazonEKS"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForElastiCache" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForElastiCache
+  name = "AWSServiceRoleForElastiCache"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForSupport" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForSupport
+  name = "AWSServiceRoleForSupport"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForElasticLoadBalancing" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForElasticLoadBalancing
+  name = "AWSServiceRoleForElasticLoadBalancing"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForTrustedAdvisor" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForTrustedAdvisor
+  name = "AWSServiceRoleForTrustedAdvisor"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForAutoScaling" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAutoScaling
+  name = "AWSServiceRoleForAutoScaling"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForRDS" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForRDS
+  name = "AWSServiceRoleForRDS"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_s3_bucket" "drift_demo_bucket_369b" {
+  # Imported from arn:aws:s3:::drift-demo-bucket-369b
+  name = "drift-demo-bucket-369b"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForOrganizations" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForOrganizations
+  name = "AWSServiceRoleForOrganizations"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForSSO" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForSSO
+  name = "AWSServiceRoleForSSO"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForAmazonEKSNodegroup" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAmazonEKSNodegroup
+  name = "AWSServiceRoleForAmazonEKSNodegroup"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForResourceExplorer" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForResourceExplorer
+  name = "AWSServiceRoleForResourceExplorer"
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
