98f56e1781924f940a0e7c0c3881d84f9f164ef2
fekri600 <30121172+fekri600@users.noreply.github.com>
2025-09-15 03:21:10 +0000

    chore(import): add generated to-import files and module hook in main.tf

---
+++ to-import/blocks-to-import.tf
@@ -0,0 +1,209 @@
+resource "aws_iam_role" "AWSServiceRoleForAmazonEKS" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAmazonEKS
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForElastiCache" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForElastiCache
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForSupport" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForSupport
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForElasticLoadBalancing" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForElasticLoadBalancing
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForTrustedAdvisor" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForTrustedAdvisor
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForAutoScaling" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAutoScaling
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForRDS" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForRDS
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForOrganizations" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForOrganizations
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForSSO" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForSSO
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
+
+resource "aws_iam_role" "AWSServiceRoleForAmazonEKSNodegroup" {
+  # Imported from arn:aws:iam::490004637046:role/AWSServiceRoleForAmazonEKSNodegroup
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = all
+  }
+}
